35b812af54c09ec6c6473f6eb6bbfd29e200c561
Hasan75 <hasanjab14@gmail.com>
2021-09-02 05:06:40 +0600

    search result added

--- js/app.js
+++ js/app.js
@@ -1,14 +1,26 @@
 const searchBook = () => {
     const searchInput = document.getElementById('search-input');
     const searchText = searchInput.value;
-    searchInput.value = ''
+    searchInput.value = '';
 
-    const url = `https://openlibrary.org/search.json?q=${searchText}`
+    if(searchText === ''){
+        console.log('error');
+        document.getElementById('search-result-counter').textContent = ''
+    }
+    else{
+      const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
     .then(res => res.json())
     .then(data => displaySearchResutl(data.docs))
-    .catch(error => console.log(error))
+    .catch(error => console.log(error))  
+    }
+    
 }
 const displaySearchResutl = books => {
-    console.log(books)
+    console.log(books.length)
+    const resultContainer = document.getElementById('result-container');
+    document.getElementById('search-result-counter').innerHTML = `
+    <p class="text-white p-3 mb-5 bg-body text-center border rounded">${books.length} Search Results Found </p>
+    `;
+
 }
