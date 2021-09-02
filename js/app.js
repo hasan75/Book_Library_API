a30917f550ecb8b538608d8982e1385e8f7a99d3
Hasan75 <hasanjab14@gmail.com>
2021-09-02 10:06:03 +0600

    all the functionalities checked & error function modified

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const toggleSpinner = displayStyle => {
     document.getElementById('spinner').style.display = displayStyle;
 }
 const toggleSearchResult = displayStyle => {
-    document.getElementById('result-container').style.display = displayStyle;
+    document.getElementById('whole-result').style.display = displayStyle;
     document.getElementById('search-result-counter').style.display = displayStyle;
 }
 
@@ -20,13 +20,13 @@ const searchBook = () => {
 
     if(searchText === ''){
         displayError('Please type something to search.');
-        document.getElementById('search-result-counter').textContent = '';
         toggleSpinner('none');
+        toggleSearchResult('none');
     }
     else{
-        document.getElementById('display_error').textContent = '';
+    document.getElementById('display_error').textContent = '';
 
-      const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
     fetch(url)
     .then(res => res.json())
     .then(data => displaySearchResutl(data.docs))
@@ -47,10 +47,13 @@ const displaySearchResutl = books => {
     const resultContainer = document.getElementById('result-container');
     // setting search result amount 
     document.getElementById('search-result-counter').innerHTML = `
-    <p class="text-white p-3 mb-5 bg-body text-center border rounded">${books.length} Search Results Found </p>
+    <p class="text-white fw-bolder p-3 mb-5 bg-body text-center border rounded">${books.length} Search Results Found </p>
     `;
     // clearing search result after a search 
     resultContainer.textContent = '';
+    if(!books.length){
+        displayError('No Result Found. Please Search with Another Keyword');
+    }
     //showing all the book resluts
     books?.forEach(book => {
         console.log(book)
@@ -58,21 +61,19 @@ const displaySearchResutl = books => {
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-            <img height="350px" src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : 10909258}-M.jpg" class="card-img-top" alt="...">
+            <img height="370px" src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : 10909258}-M.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${book.title}</h5>
-                <ul class="card-text">
-                    <li>Author: ${book.author_name[0] ? book.author_name[0] : 'Author Not Found'}</li>
-                    <li>Publisher: ${book.publisher[0] ? book.publisher[0] : 'Publisher Not Found'}</li>
+                <ul class="card-text my-2">
+                    <li>Author: <span class="fw-bolder"> ${book.author_name ? book.author_name : 'Author Not Found'} </span></li>
+                    <li>Publisher: ${book.publisher ? book.publisher : 'Publisher Not Found'}</li>
                     <li>1st Publish Year: ${book.first_publish_year ? book.first_publish_year : 'First Publish Year Not Found'}</li>
                 </ul>
             </div>
         </div>
         `;
         resultContainer.appendChild(div);
-        
     });
     toggleSpinner('none');
     toggleSearchResult('block');
-
 }
