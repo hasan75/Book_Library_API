482a7b682e684dc724e5aa0f0d8019178db4d4ab
Hasan75 <hasanjab14@gmail.com>
2021-09-02 06:34:40 +0600

    image replaced

--- js/app.js
+++ js/app.js
@@ -21,17 +21,20 @@ const displaySearchResutl = books => {
     document.getElementById('search-result-counter').innerHTML = `
     <p class="text-white p-3 mb-5 bg-body text-center border rounded">${books.length} Search Results Found </p>
     `;
+    // clearing search result after a search 
+    resultContainer.textContent = '';
+    //showing all the book resluts
     books?.forEach(book => {
         console.log(book)
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+            <img height="350px" src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : 10909258}-M.jpg" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${book.title}</h5>
                 <ul class="card-text">
-                    <li>Author: ${book.author_name[0]}</li>
+                    <li>Author: ${book.author_name[0] ? book.author_name[0]: 'Author Not Found'}</li>
                     <li>Publisher: ${book.publisher[0]}</li>
                     <li>1st Publish Year: ${book.first_publish_year}</li>
                 </ul>
